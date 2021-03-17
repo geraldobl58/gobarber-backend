0c041998c1d79883f496e61dbdcf436a62e33372
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-17 11:08:47 -0300

    feat: Relationship in the models

--- src/database/migrations/1615983063604-CreateAppointments.ts
+++ src/database/migrations/1615983063604-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1615983063604 implements MigrationInterfa
           columns: [
             {
               name: 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()'
